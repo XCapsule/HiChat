d5e871034104bf6f114f3aeaf293bda7b7d081ce
陈昊 <vince@chenhaodeMacBook-Pro.local>
2017-07-15 17:05:38 +0800

    HiChatv1.1.0

--- HiChat/www/scripts/hichat.js
+++ HiChat/www/scripts/hichat.js
@@ -14,8 +14,43 @@ var HiChat = function() {
 };
 
 var Users=[];
+// 本地储存的内容的时间,大于一天(86400000)就重新登录
+var lastTimeLogin=localStorage.lastTimeLogin;
+var thisTime=new Date().getTime();
+var storeAge=thisTime-lastTimeLogin;//获取当前储存的已存活时间
+var localname=localStorage.localname;
+// 1为可以登录
+if(localStorage.statuscode=='NaN'){localStorage.statuscode=0}
+localStorage.statuscode-=(-1);
+function login(that){
+            var nickName=localname;
+            if(nickName.trim().length!=0){
+                that.socket.emit('login',nickName);
+            }else{
+                document.getElementById('nicknameInput').focus();
+            }
+}
+// 关闭页面时的相应操作
+window.onbeforeunload=onclose;
+function onclose(){
+localStorage.statuscode-=1;
+localStorage.lastTimeLogin=new Date().getTime();
+}
 
 
+// 设置循环显示用户的数量
+setInterval(function(){
+                var StatusDom=document.getElementById('status')
+                var infoclasslist=StatusDom.classList;
+                StatusDom.style.display='none';
+                infoclasslist.remove('fadeInUp','fadeOutUp');
+                StatusDom.innerHTML='当前在线人数'+"<font color='#f17c67'>"+Users.length+'</font>'+'人';
+                infoclasslist.add('fadeInUp');
+                StatusDom.style.display='inline-block';
+                setTimeout(function () {
+                infoclasslist.add('fadeOutUp');
+            },5000);
+            },30000);
 
 // prototype原型链多个子类复用同一个，不会新建。
 HiChat.prototype={
@@ -23,13 +58,23 @@ HiChat.prototype={
         var that=this;
         this.socket=io.connect();
         this.socket.on('connect',function(){
-        document.getElementById('info').textContent = '请输入你的昵称 *_*  ';
-        document.getElementById('nickWrapper').style.display = 'block';
-        document.getElementById('nicknameInput').focus();      
+            if(localname&&(storeAge<86400000)){
+                // 通过localstorage登陆
+                if(localStorage.statuscode==1){
+                    localStorage.lastTimeLogin=thisTime;
+                login(that)}
+                else
+                    document.getElementById('info').textContent = '您已打开多个窗口！';
+                    
+            }
+            else{
+                document.getElementById('info').textContent = '请输入你的昵称 *_*  ';
+                document.getElementById('nickWrapper').style.display = 'block';
+                document.getElementById('nicknameInput').focus(); }     
         })
         this.socket.on('loginSuccess',function () {
             document.title='嗨聊|'+document.getElementById('nicknameInput').value;
-            document.getElementById('loginWrapper').style.display = 'none';
+            document.getElementById('loginWrapper').classList.add('animated','slideOutUp');
             document.getElementById('messageInput').focus();
         })
         this.socket.on('nickNull',function () {
@@ -53,16 +98,6 @@ HiChat.prototype={
             },5000);
             },400);
 
-            setInterval(function(){
-                StatusDom.style.display='none';
-                infoclasslist.remove('fadeInUp','fadeOutUp');
-                StatusDom.innerHTML='当前在线人数'+"<font color='#f17c67'>"+Users.length+'</font>'+'人';
-                infoclasslist.add('fadeInUp');
-                StatusDom.style.display='inline-block';
-                setTimeout(function () {
-                infoclasslist.add('fadeOutUp');
-            },5000);
-            },20000);
 
 
         })
@@ -79,12 +114,8 @@ HiChat.prototype={
         })
         // 登陆
         document.getElementById('loginBtn').addEventListener('click',function(){
-            var nickName=document.getElementById('nicknameInput').value;
-            if(nickName.trim().length!=0){
-                that.socket.emit('login',nickName);
-            }else{
-                document.getElementById('nicknameInput').focus();
-            }
+                localname=localStorage.localname=document.getElementById('nicknameInput').value;
+                login(that);
         },false);
         // 发送消息,
         document.getElementById('sendBtn').addEventListener('click',function(){
